3f78d05232967773a891a9582ba16564fa02e3aa
Neel Thomas <neeltom92@gmail.com>
2019-08-11 19:11:19 +0530

    go_aws_list_instance.go
    //AWS Golang code to list all instance under a specific region based on aws config

---
+++ go_aws_list_instance.go
@@ -0,0 +1,49 @@
+//AWS Golang code to list all instance under a specific region based on aws config
+
+
+package main
+
+import (
+    "fmt"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/ec2"
+)
+
+
+func main() {
+    // Load session from shared config
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+
+    svc := ec2.New(sess)
+
+
+    filters := []*ec2.Filter{
+    		&ec2.Filter{
+    			Name: aws.String("tag:Name"),
+    			Values: []*string{
+    				aws.String("*"),
+    			},
+    		},
+    	}
+
+
+    	input := ec2.DescribeInstancesInput{Filters: filters}
+      result, err := svc.DescribeInstances(&input)
+      if err != nil {
+		panic(err.Error())
+	}
+
+
+  for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+
+         fmt.Printf("%s", *instance.InstanceId)
+         fmt.Println()
+		}
+	}
+
+
+}
